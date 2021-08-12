f293260452ea6cb707c0aa82eab756cad6a33ee3
feophan <>
2021-08-12 17:12:04 +0200

    Deployed 9cf7606 with MkDocs version: 1.2.2

--- js/extra.js
+++ js/extra.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
     table.classList.add("docutils");
   });
 });
+
+document.querySelectorAll(".ref").forEach(function(item, i) {
+	$(item).attr('id', $(item).attr('id') + "." + parseInt(i + 1));
+});
+
+document.querySelectorAll(".exp").forEach(function(item, i) {
+	item.closest("li").style.listStyleType = "none";
+	$(item).attr('id', "exp." + parseInt(i + 1));
+});
